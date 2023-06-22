6740e0d0f4b3439f765cabbdfdbd913103f44e18
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-06-21 21:29:22 -0300

    beecrowd 1001 C++.

---
+++ c++/bee_1001.cpp
@@ -0,0 +1,22 @@
+/*  The Felps Code
+*   Tico e Teco: @thefelps.code
+*   YT: @thefelpscode     
+*/ 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int A, B, X;
+
+  cin >> A;
+  cin >> B;
+
+  X = A + B;
+
+  cout << "X = " << X <<  "\n";
+
+  return 0;
+}
