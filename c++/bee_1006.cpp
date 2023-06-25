43ff94b24789a3a325b8ebec23d2a2824891b064
Felipe Sousa <felipeluis.felpssousa@gmail.com>
2023-06-25 19:18:14 -0300

    beecrowd 1006 C++ success.

---
+++ c++/bee_1006.cpp
@@ -0,0 +1,29 @@
+/*  The Felps Code
+*   GitHub: https://github.com/FelpsSousa/
+*   In: https://www.linkedin.com/in/felipeluis-felpssousa/
+*   Tico e Teco: @thefelps.code
+*   YT: @thefelpscode
+*   GitHub:      
+*/ 
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double a, b, c, media;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    media = (( 2*a ) + ( 3*b ) + ( 5*c )) / 10;
+
+    cout << "MEDIA = ";
+    cout << fixed << setprecision(1);
+    cout << media << endl;
+
+    return 0;
+}
