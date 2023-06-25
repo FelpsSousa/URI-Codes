bfcbffad4d827eae15c14ef9ec1578fe399e0063
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-06-25 11:53:03 -0300

    becrowd 1003 C++ success.

---
+++ c++/bee_1003.cpp
@@ -0,0 +1,25 @@
+/*  The Felps Code
+*   Tico e Teco: @thefelps.code
+*   YT: @thefelpscode     
+*/ 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int A, B, SOMA;
+
+  cin >> A;
+  cin >> B;
+
+  SOMA = A + B;
+
+  cout << "SOMA = ";
+  cout << SOMA << endl;
+
+  return 0;
+}
+
+
