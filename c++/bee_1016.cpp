b26db5a93d8befa3440f4a626aec264d73799d0b
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-06-27 22:42:31 -0300

    beecrowld 1016 C++ success.

---
+++ c++/bee_1016.cpp
@@ -0,0 +1,25 @@
+/*  The Felps Code
+*   GitHub: https://github.com/FelpsSousa/
+*   In: https://www.linkedin.com/in/felipeluis-felpssousa/
+*   Tico e Teco: @thefelps.code
+*   YT: @thefelpscode
+*/
+
+// Distancia Entre Dois Carros
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int km, minutos;
+
+  cin >> km;
+
+  minutos = km * 2;
+
+  cout << minutos << " minutos" << endl;
+
+  return 0;
+}
