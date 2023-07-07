d4a069466381b74de484cd0fe6016c3a549f0aaa
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-07-07 06:46:52 -0300

    beecrowld 3055 C++ success.

---
+++ c++/bee_3055.cpp
@@ -0,0 +1,25 @@
+/*  The Felps Code
+*   GitHub: https://github.com/FelpsSousa/
+*   LinkedIn: https://www.linkedin.com/in/felipeluis-felpssousa/
+*   TikTok: @thefelps.code
+*   YT: @thefelpscode
+*/
+
+// NOTA ESQUECIDA - INICIANTE - NIVEL 1 | +1.1 PONTOS.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int nota1, nota2, media;
+
+  cin >> nota1 >> media;
+
+  nota2 = (2 * media) - nota1; 
+
+  cout << nota2 << endl;
+
+  return 0;
+}
