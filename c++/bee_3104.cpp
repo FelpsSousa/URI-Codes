6437c00a0595775894bc3d08cfe3e8ad44adceeb
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-07-11 21:09:51 -0300

    beecrowld 3104 C++ success. string -> int %...

--- c++/bee_3104.cpp
+++ c++/bee_3104.cpp
@@ -5,7 +5,15 @@
 *   YT: @thefelpscode
 */
 
-// RESTO 2.0 - MATEMATICA - NIVEL 10 | +10.5 PONTOS.
+//  RESTO 2.0 - MATEMATICA - NIVEL 10 | +10.5 PONTOS.
+
+/*
+*   Neste codigo, o numero A eh lido como uma string e o B eh lido como um inteiro.
+*   Em seguida, eh realizado um loop pela string A para calcular o resto da divisao.
+*   A cada iteracao, o digito atual eh convertido em um numero inteiro subtraindo o valor ASCII de '0',
+*   e eh adicionado ao valor acumulado do resto. Em cada iteracao, o resto eh atualizado utilizando o 
+*   operador de modulo '%'. Finalizando com a impressao da saida.
+*/
 
 #include <iostream>
 
@@ -13,10 +21,19 @@ using namespace std;
 
 int main()
 {
-  int A, B;
+  string A;
+  int B;
+
   cin >> A >> B;
 
-  cout << (A % B) << endl;
+  int resto = 0;
+
+  for (char c : A)
+  {
+    resto = (resto * 10 + (c - '0')) % B;
+  }
+
+  cout << resto << endl;
 
   return 0;
 }
