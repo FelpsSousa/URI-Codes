2d4536985d551e496678b616034b7c70a184c735
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-07-11 21:02:17 -0300

    beecrowd 3104 C++. Wrong 80%. Trying a new solution...

---
+++ c++/bee_3104.cpp
@@ -0,0 +1,22 @@
+/*  The Felps Code
+*   GitHub: https://github.com/FelpsSousa/
+*   LinkedIn: https://www.linkedin.com/in/felipeluis-felpssousa/
+*   TikTok: @thefelps.code
+*   YT: @thefelpscode
+*/
+
+// RESTO 2.0 - MATEMATICA - NIVEL 10 | +10.5 PONTOS.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int A, B;
+  cin >> A >> B;
+
+  cout << (A % B) << endl;
+
+  return 0;
+}
