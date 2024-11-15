25c01eb22699d08891890f683e41a84160ab9b4d
felipefelpsrs <felipefelpsrs@gmail.com>
2024-11-15 15:39:42 -0300

    beecrowld 1021 C++ success.

--- c++/bee_1021.cpp
+++ c++/bee_1021.cpp
@@ -1,43 +1,42 @@
 /*  The Felps Code
 *   GitHub: https://github.com/FelpsSousa/
 *   In: https://www.linkedin.com/in/felipeluis-felpssousa/
-*   Tico e Teco: @thefelps.code
+*   TikTok: @thefelps.code
 *   YT: @thefelpscode
 */
 
 // Notas e Moedas
 
+#include <stdio.h>
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
-
-int main()
-{
-  double valor;
-
-  cin >> valor;
-
-  int     notas[6]  = { 100, 50, 20, 10, 5, 2 };
-  double  moedas[6] = { 1.0, 0.5, 0.25, 0.10, 0.05, 0.01 };
-
-  cout << "NOTAS:" << endl;
-
-  for ( int i=0; i<6; i++ )
-  {
-    int quantidadeNotas = ( valor / notas[i] );
-    valor -= ( quantidadeNotas * notas[i] );
-    cout << quantidadeNotas << " nota(s) de R$ " << notas[i] << ".00" << endl;
-  }
-
-  cout << "MOEDAS:" << endl;
-
-  for ( int i=0; i<6; i++ )
-  {
-    int quantidadeMoedas = ( valor / moedas[i] );
-    valor -= ( quantidadeMoedas * moedas[i] );
-    cout << quantidadeMoedas << " moeda(s) de R$ " << fixed << setprecision(2) << moedas[i] << endl;
-  }
-
-  return 0;
+ 
+int main() {
+ 
+    double N;
+    cin >> N;
+    
+    int valor = round(N * 100);
+    
+    int notas[] = {10000, 5000, 2000, 1000, 500, 200};
+    int moedas[] = {100, 50, 25, 10, 5, 1};
+    
+    cout << "NOTAS:" << endl;
+    for (int nota : notas) {
+        int qtd = valor / nota;
+        cout << qtd << " nota(s) de R$ " << fixed << setprecision(2) << (nota / 100.0) << endl;
+        valor %= nota;
+    }
+    
+    cout << "MOEDAS:" << endl;
+    for (int moeda : moedas) {
+        int qtd = valor / moeda;
+        cout << qtd << " moeda(s) de R$ " << fixed << setprecision(2) << (moeda / 100.0) << endl;
+        valor %= moeda;
+    }
+ 
+    return 0;
 }
