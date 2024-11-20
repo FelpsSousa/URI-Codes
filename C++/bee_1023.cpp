741018256248c360f40f4d7f2337cf3dd525e8fd
felipefelpsrs <felipefelpsrs@gmail.com>
2024-11-20 15:15:57 -0300

    beecrowd 1023 C++ wrong answer (100%) [2]

--- C++/bee_1023.cpp
+++ C++/bee_1023.cpp
@@ -5,75 +5,71 @@
  * License: MIT
  * Description: BEE 1023.
  * Date: 2024-11-20
- * Version: 1.0
+ * Version: 2.0
  */
 
 #include <iostream>
 #include <vector>
+#include <map>
 #include <cmath>
 #include <iomanip>
-#include <algorithm>
 
 using namespace std;
 
 struct Property {
-  int residents;
-  int consumption;
+    int residents;
+    int consumption;
 };
 
 void processCity(int cityNumber, vector<Property>& properties) {
-  int totalResidents = 0;
-  int totalConsumption = 0;
+    int totalResidents = 0;
+    int totalConsumption = 0;
 
-  vector<pair<int, int>> peopleConsumption;
+    map<int, int> groupedConsumption;
 
-  for (const auto& property : properties) {
-    totalResidents += property.residents;
-    totalConsumption += property.consumption;
+    for (const auto& property : properties) {
+        totalResidents += property.residents;
+        totalConsumption += property.consumption;
 
-    int avgConsumption = floor(property.consumption / property.residents);
-    peopleConsumption.emplace_back(property.residents, avgConsumption);
-  }
-
-  sort(peopleConsumption.begin(), peopleConsumption.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
-    if (a.second == b.second) {
-      return a.first < b.first;
+        int avgConsumption = property.consumption / property.residents;
+        groupedConsumption[avgConsumption] += property.residents;
     }
-    return a.second < b.second;
-  });
 
-  cout << "Cidade# " << cityNumber << ":\n";
-  for (size_t i = 0; i < peopleConsumption.size(); i++) {
-    if (i > 0) cout << " ";
-    cout << peopleConsumption[i].first << "-" << peopleConsumption[i].second;
-  }
-  cout << "\n";
+    cout << "Cidade# " << cityNumber << ":\n";
 
-  double avgTotalConsumption = static_cast<double>(totalConsumption) / totalResidents;
-  cout << fixed << setprecision(2) << "Consumo medio: " << avgTotalConsumption << " m3.\n";
+    bool first = true;
+    for (const auto& entry : groupedConsumption) {
+        if (!first) cout << " ";
+        cout << entry.second << "-" << entry.first;
+        first = false;
+    }
+    cout << "\n";
+
+    double avgTotalConsumption = static_cast<double>(totalConsumption) / totalResidents;
+    cout << fixed << setprecision(2) << "Consumo medio: " << avgTotalConsumption << " m3.\n";
 }
 
 int main() {
-  int cityNumber = 1;
+    int cityNumber = 1;
 
-  while (true) {
-    int n;
-    cin >> n;
+    while (true) {
+        int n;
+        cin >> n;
 
-    if (n == 0) break;
+        if (n == 0) break;
 
-    vector<Property> properties;
+        vector<Property> properties;
 
-    for (int i = 0; i < n; i++) {
-      int residents, consumption;
-      cin >> residents >> consumption;
-      properties.push_back({residents, consumption});
+        for (int i = 0; i < n; ++i) {
+            int residents, consumption;
+            cin >> residents >> consumption;
+            properties.push_back({residents, consumption});
+        }
+
+        if (cityNumber > 1) cout << "\n";
+        processCity(cityNumber, properties);
+        cityNumber++;
     }
 
-    if (cityNumber > 1) cout << "\n";
-    processCity(cityNumber, properties);
-    cityNumber++;
-  }  
-  
-  return 0;
+    return 0;
 }
