08c508c00c0c7d150cc1a757620c8fb9f74da382
felipefelpsrs <felipefelpsrs@gmail.com>
2024-11-21 18:27:40 -0300

    beecrowd 2615 SQL accepted

---
+++ PostgreSQL/bee_2615.sql
@@ -0,0 +1,13 @@
+/*  
+ * Author: Felipe Luis Rodrigues Sousa (FelipeFelpsCode - Plataforma URI/Beecrowd)
+ * GitHub: https://github.com/FelpsSousa/
+ * Repository: https://github.com/FelpsSousa/URI-Codes
+ * License: MIT
+ * Description: BEE 2615.
+ * Date: 2024-11-21
+ * Version: 1.0
+ */
+
+SELECT DISTINCT city
+FROM customers
+ORDER BY city;
