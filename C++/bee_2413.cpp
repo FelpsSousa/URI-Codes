a1a73ff11fe66a95aa7cd0124a58e81900c673a5
felipefelpsrs <felipefelpsrs@gmail.com>
2024-11-21 13:47:56 -0300

    beecrowd 2413 C++ accepted

---
+++ C++/bee_2413.cpp
@@ -0,0 +1,25 @@
+/*  
+ * Author: Felipe Luis Rodrigues Sousa (FelipeFelpsCode - Plataforma URI/Beecrowd)
+ * GitHub: https://github.com/FelpsSousa/
+ * Repository: https://github.com/FelpsSousa/URI-Codes
+ * License: MIT
+ * Description: BEE 2413.
+ * Date: 2024-11-21
+ * Version: 1.0
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int t;
+
+  cin >> t;
+
+  int firstLink = 4 * t;
+
+  cout << firstLink << endl;
+
+  return 0;
+}
