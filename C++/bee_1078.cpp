c6a852f90a987381ade07153e4f73fa7c07b36dc
felipefelpsrs <felipefelpsrs@gmail.com>
2024-11-21 13:14:01 -0300

    beecrowd 1078 C++ accepted

---
+++ C++/bee_1078.cpp
@@ -0,0 +1,25 @@
+/*  
+ * Author: Felipe Luis Rodrigues Sousa (FelipeFelpsCode - Plataforma URI/Beecrowd)
+ * GitHub: https://github.com/FelpsSousa/
+ * Repository: https://github.com/FelpsSousa/URI-Codes
+ * License: MIT
+ * Description: BEE 1078.
+ * Date: 2024-11-21
+ * Version: 1.0
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+
+  cin >> n;
+
+  for (int i = 1; i <= 10; i++) {
+    cout << i << " x " << n << " = " << i * n << endl;
+  }
+
+  return 0;
+}
