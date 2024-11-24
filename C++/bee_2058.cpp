c642e7cb2279df68783bd003d6a75da1b73fae91
felipefelpsrs <felipefelpsrs@gmail.com>
2024-11-24 11:32:07 -0300

    beecrowd 2058 C++ accepted

---
+++ C++/bee_2058.cpp
@@ -0,0 +1,25 @@
+/*  
+ * Author: Felipe Luis Rodrigues Sousa (FelipeFelpsCode - Plataforma URI/Beecrowd)
+ * GitHub: https://github.com/FelpsSousa/
+ * Repository: https://github.com/FelpsSousa/URI-Codes
+ * License: MIT
+ * Description: BEE 2058.
+ * Date: 2024-11-24
+ * Version: 1.0
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long long N;
+  
+  cin >> N;
+
+  long long triangles = N - 2;
+
+  cout << triangles << endl;
+
+  return 0;
+}
