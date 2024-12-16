eec8adedc4673765b17aee190b083905a9cf03c5
felipefelpsrs <felipefelpsrs@gmail.com>
2024-12-16 20:54:05 -0300

    beecrowd 3091 C++ accepted

---
+++ C++/bee_3091.cpp
@@ -0,0 +1,25 @@
+/*  
+ * Author: Felipe Luis Rodrigues Sousa (FelipeFelpsCode - Platform URI/Beecrowd)
+ * GitHub: https://github.com/FelpsSousa/
+ * Repository: https://github.com/FelpsSousa/URI-Codes
+ * License: MIT
+ * Description: BEE 3091
+ * Date: 2024-12-16
+ * Version: 1.0
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int A, B;
+
+  cin >> A >> B;
+
+  int remainder = A % B;
+
+  cout << remainder << endl;
+
+  return 0;
+}
