d4cddd221ca2e0ebf9ed4124e1f66acea2ae90d9
felipefelpsrs <felipefelpsrs@gmail.com>
2024-12-17 20:58:53 -0300

    beecrowd 2483 C++ accepted

---
+++ C++/bee_2483.cpp
@@ -0,0 +1,27 @@
+/*  
+ * Author: Felipe Luis Rodrigues Sousa (FelipeFelpsCode - Platform URI/Beecrowd)
+ * GitHub: https://github.com/FelpsSousa/
+ * Repository: https://github.com/FelpsSousa/URI-Codes
+ * License: MIT
+ * Description: BEE 2483
+ * Date: 2024-12-17
+ * Version: 1.0
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int I;
+
+  cin >> I;
+
+  cout << "Feliz nat";
+  for (int i = 0; i < I; i++) {
+    cout << "a";
+  }
+  cout << "l!" << endl;
+
+  return 0;
+}
