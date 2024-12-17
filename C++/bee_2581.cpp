21dcf6440a15c968b60dea9d6e3c7aebfbe1e0f7
felipefelpsrs <felipefelpsrs@gmail.com>
2024-12-17 20:49:41 -0300

    beecrowd 2581 C++ accepted

---
+++ C++/bee_2581.cpp
@@ -0,0 +1,30 @@
+/*  
+ * Author: Felipe Luis Rodrigues Sousa (FelipeFelpsCode - Platform URI/Beecrowd)
+ * GitHub: https://github.com/FelpsSousa/
+ * Repository: https://github.com/FelpsSousa/URI-Codes
+ * License: MIT
+ * Description: BEE 2581
+ * Date: 2024-12-17
+ * Version: 1.0
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int N;
+
+  cin >> N;
+  cin.ignore();
+
+  string question;
+
+  for (int i = 0; i < N; i++) {
+    getline(cin, question);
+    cout << "I am Toorg!" << endl;
+  }
+
+  return 0;
+}
