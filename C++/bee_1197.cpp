e74c8f3d7ea97dd496fc124bc6fa8146bffb39a7
felipefelpsrs <felipefelpsrs@gmail.com>
2025-01-05 20:50:23 -0300

    beecrowd 1197 C++ accepted

---
+++ C++/bee_1197.cpp
@@ -0,0 +1,24 @@
+/*  
+ * Author: Felipe Luis Rodrigues Sousa (FelipeFelpsCode - Platform URI/Beecrowd)
+ * GitHub: https://github.com/FelpsSousa/
+ * Repository: https://github.com/FelpsSousa/URI-Codes
+ * License: MIT
+ * Description: BEE 1197
+ * Date: 2025-01-05
+ * Version: 1.0
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int v, t;
+
+  while (cin >> v >> t) {
+    cout << 2 * v * t << endl;
+  }
+
+  return 0;
+}
+
