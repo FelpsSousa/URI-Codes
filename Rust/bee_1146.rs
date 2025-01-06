17e6668298c3750a5fe8c01be48e6f2f0d3f1201
felipefelpsrs <felipefelpsrs@gmail.com>
2025-01-06 20:58:07 -0300

    beecrowd 1146 Rust compilation error

---
+++ Rust/bee_1146.rs
@@ -0,0 +1,36 @@
+/*  
+ * Author: Felipe Luis Rodrigues Sousa (FelipeFelpsCode - Platform URI/Beecrowd)
+ * GitHub: https://github.com/FelpsSousa/
+ * Repository: https://github.com/FelpsSousa/URI-Codes
+ * License: MIT
+ * Description: BEE 1146
+ * Date: 2025-01-06
+ * Version: 1.0
+ */
+
+use std::io;
+
+fn main() {
+  let mut input = String::new();
+
+  loop {
+    input.clear();
+
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+
+    let x: i32 = match input.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        continue,
+      }
+    };
+
+    if x == 0 {
+      break;
+    }
+
+    let sequence: Vec<String> = (1..=x).map(|n| n.to_string()).collect();
+
+    println!("{}", sequence.join(" "));
+  }
+}
